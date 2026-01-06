872222b1225af84261e25e7ebe680b4f30d0d1d0
tninan <tninan.optimus@yobelscm.biz>
2026-01-05 23:32:01 -0500

    Estructura del backend

---
+++ src/main/java/com/topsell/backend/TopsellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.topsell.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopsellBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topsell/backend/TopsellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topsell.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopsellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
