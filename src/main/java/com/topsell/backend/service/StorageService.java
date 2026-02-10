8f5e5ca691dbcffe542f081fe21631551149e9c0
Tomás Ninán <tninan.optimus@yobelscm.biz>
2026-02-10 16:26:55 -0500

    configuración para las imagenes

--- src/main/java/com/topsell/backend/service/StorageService.java
+++ src/main/java/com/topsell/backend/service/StorageService.java
@@ -15,9 +15,9 @@ public class StorageService {
     private final Cloudinary cloudinary;
 
     public StorageService(
-            @Value("${cloudinary.cloud_name}") String cloudName,
-            @Value("${cloudinary.api_key}") String apiKey,
-            @Value("${cloudinary.api_secret}") String apiSecret) {
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
